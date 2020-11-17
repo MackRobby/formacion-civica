79a79c8bb5c95d277025d5cee22ef0eb07f5e74e
Chema <roblesjm@gmail.com>
2020-11-17 16:40:29 +0100

    cambio requerido por cliente PROY-004

--- ddl.sql
+++ ddl.sql
@@ -1,2 +1,2 @@
 -- CREA TABLE ventas
-CREATE TABLE sales (id int primary key, price decimal(10,2) tax decimal(10, 2));
+CREATE TABLE sales (id int primary key, price decimal(10,2) tax decimal(4, 2), comment varchar(200));
