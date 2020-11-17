1b842b0ede04f341dc140a422d08ecaa77bc7df4
Chema <roblesjm@gmail.com>
2020-11-17 16:51:01 +0100

    ADD: tabla cliente

--- ddl.sql
+++ ddl.sql
@@ -2,3 +2,7 @@
 CREATE TABLE sales (id int primary key, price decimal(10,2) tax decimal(4, 2), comment varchar(200));
 -- CREAR VISTA
 CREATE VIEW vw_sales SELECT id, price FROM SALES;
+
+-- CREAR TABLA cliente
+
+CREATE TABLE client (id int primary key, name varchar(250) not null);
