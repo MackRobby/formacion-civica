f9ad79b6b8a350da1fd435cf5de2fb94ceb320c1
Jose Morales <jmmoralespinola@gmail.com>
2020-11-18 16:45:57 +0100

    Add venta - precios

--- ddl.sql
+++ ddl.sql
@@ -1,5 +1,5 @@
 -- CREA TABLE ventas
-CREATE TABLE sales (id int primary key, price decimal(10,2) tax decimal(4, 2), comment varchar(200));
+CREATE TABLE venta (id int primary key, precios decimal(10,2) tax decimal(4, 2), comment varchar(200));
 -- CREAR VISTA
 CREATE VIEW vw_sales SELECT id, price FROM SALES;
 
